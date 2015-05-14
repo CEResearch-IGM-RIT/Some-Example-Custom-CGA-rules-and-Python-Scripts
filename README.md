96d4846b096614b802ef3fc015756131613a144d
kaszagrzegorz <kasza.grzegorz@gmail.com>
2015-05-14 12:46:21 +0200

    added blog url to README 	OGR_aggregate.py restructured as set of functions

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CityEngine
 Custom CGA rules and Python scripts
+Most are described in detail on [!my blog](http://kaszagrzegorz.github.io/index_en.html)
--- scripts/OGR_aggregate.py
+++ scripts/OGR_aggregate.py
@@ -1,5 +1,5 @@
 '''
-Created on 25-04-2015
+Created on 14-05-2015
 
 @author: kaszagrzegorz
 '''
@@ -8,108 +8,142 @@ import os
 import sys
 import re
 import csv
+
 # get a CityEngine instance
 ce = CE()
 
-for pliki in ce.getObjectsFrom(ce.toFSPath("scripts")+'/geometria/', ce.isFile):
-    ce.delete(pliki)
-print 'wyczyszczono katalog roboczy'
-#pusta zmienna, którą będziemy zapełniać listą wierzchołków w formacie WKT. znak /n oznacza załamanie linii
-geometria_WKT ='WKT,\n'
+#ścieżka systemowa do katalogu roboczego
+tempdir = ce.toFSPath("scripts")+"/temp/"
 
-#zmienna zawierająca geometrię na której operujemy
-zaznaczenie = ce.selection()
-obiekty = ce.getObjectsFrom(zaznaczenie)
-print "zaznaczono",len(obiekty),"obiektow"
-#iterujemy przez zaznaczone obiekty i każdy tłumaczymy na WKT
-for geometria in obiekty:
-    geometria_WKT +='"POLYGON (('
-    #zmienna zawierająca ilość wierzchołków geometrii 
-    ilosc_wierzcholkow = (len(ce.getVertices(geometria))/3) 
-    #zmienna zawierająca zbiór wierzchołków XYZ
-    wierzcholki = ce.getVertices(geometria)    
-    #iterujemy przez wierzchołki i odczytujemy tylko wartości X i Y, pomijamy Z
-    for n in range(ilosc_wierzcholkow):
-        #formuła, która dla każdego n-tego wierzchołka bierze tylko współrzędną 1 i 3, pomijając 2 która jest wysokością
-        x = ce.getVertices(geometria)[3*(n+1)-3]
-        x = round(x,2)
-        #cityengine przechowuje współrzędną Y z minusem. Dla zgodności z szejpami zewnętrzymi trzeba przemnożyć przez -1. Uwaga: przy ładowaniu geometrii z powrotem do CityEngine trzeba przemnozyć przez -1 jeszcze raz
-        y = ce.getVertices(geometria)[3*(n+1)-1]*(-1)
-        y = round(y,2)
-        #dołączamy do zmiennej kolejno wspólrzędną x, spację, współrzędną y i przecinek
-        geometria_WKT +=`x`
-        geometria_WKT +=" "
-        geometria_WKT +=`y`
-        geometria_WKT +=","    
-    #ucinamy ostatni przecinek    
-    geometria_WKT = geometria_WKT[:-1]
-    #zamykamy geometrię dwoma nawiasami i cudzysłowiem
-    geometria_WKT +='))"\n'
-#ustalamy ścieżkę zapisu i nazwę pliku csv
-plik = ce.toFSPath("scripts")+"/geometria/geometria_WKT.csv"
-#otwieramy plik z prawami zapisu
-zapis = open(plik, "w")
-#dodajemy do pliku zawartość zmiennej
-zapis.write(geometria_WKT)
-#zamykamy plik
-zapis.close()
-print 'zapisano csv'
-#ustalamy ścieżkę zapisu i nazwę pliku po konwersji do shp
-nazwa_plik_shp = 'geometria_SHP'
-plik_shp = ce.toFSPath("scripts")+'/geometria/'+nazwa_plik_shp+".shp"
-#oruchomienie zewnetrznej aplikacji ogr2ogr do konwersji csv na shp
-os.system("ogr2ogr "+plik_shp+" "+plik)
-print 'konwersja csv do shp zakonczona'
-#dodanie pustej kolumny do umożliwienia agregacji; brudny trick; lepsze byłoby agregowanie wszystkiego z pominięciem tego kroku
-os.system('ogrinfo '+plik_shp+' -sql "ALTER TABLE '+nazwa_plik_shp+' ADD COLUMN kolumna character(10)"')
-print 'dodano kolumne agregacji'
-#ustalamy ścieżkę zapisu i nazwę pliku shp po agregacji
-shp_agre = ce.toFSPath("scripts")+"/geometria/geometria_agre.shp"
-os.system("ogr2ogr -explodecollections "+shp_agre+" "+plik_shp+" -dialect sqlite -sql 'SELECT kolumna,ST_Union(geometry) AS geometry FROM "+nazwa_plik_shp+" GROUP BY kolumna'")
-print 'agregacja zakonczona'
-#ustalamy ścieżkę zapisu i nazwę pliku shp po konwersji do csv
-plik_csv_agre = ce.toFSPath("scripts")+"/geometria/geometria_WKT_agre.csv"
-#konwersja zagregowanego pliku shp do csv z geometrią WKT
-os.system("ogr2ogr -f CSV "+plik_csv_agre+" "+shp_agre+" -lco GEOMETRY=AS_WKT")
-print 'konwersja do CSV WKT zakonczona'
-#otwieramy plik z geometrią WKT
-otwarcie_WKT = open(plik_csv_agre, "r")
-#ustalenie zmiennej w której znajduje się treśc otwartego pliku CSV
-odczyt_WKT = otwarcie_WKT.read()
-#zamykamy plik, nie jest już potrzebny
-otwarcie_WKT.close()
-#zamiana linijek tekstu na elementy listy
-podzial_linii = re.compile("[^\n]+")
-#ustalenie zmiennej w której znajduje się lista zrobiona z linijek tekstu
-zbior_linii = podzial_linii.findall(odczyt_WKT)
-#usunięcie pierwszego elementu listy zawierającego ciąg znaków "WKT,kolumna,"
-zbior_linii.pop(0)
-for poligon in zbior_linii:
-    #ustalenie pustej listy zapelnianej wierzcholkami do stworzenia nowej geometrii w CityEngine
-    wierzcholki = []
-    #usuwamy zbędne ciągi znaków wynikające z konstrukcji WKT
-    poligon = poligon.replace('"POLYGON ((','')
-    poligon = poligon.replace('))",','')
-    #rozbijamy uzyskany ciąg znaków na listę wierzchołków o konstrukcji x y, x y, x y,...
-    poligon = poligon.split(',')
-    #iterujemy przez każdy wierzchołek poligonu by wydobyć współrzędne wierzchołków
-    for wierzcholek in poligon:
-        #rozbijamy konstrukcję [x y] na dwa elementy [x,y] za pomocą rozdzielającej spacji    
-        wierzcholek = wierzcholek.split(' ')
-        #konwersja tekstu (str) na float
-        wspolrzedne = [float(x) for x in wierzcholek]
-        wspolrzedna_x = wspolrzedne[0]
-        #przemnażamy z powrotem współrzędną y przez -1
-        wspolrzedna_y = wspolrzedne[1]*(-1)
-        #dodajemy wspolrzedną z
-        wspolrzedna_z = 0
-        #dodajemy do listy wspolrzednych poligonu uzyskane wspolrzedne wierzcholka. UWAGA: dodajemy w odwrotnej kolejności (yzx, zamiast xzy) oraz nie do końca listy, ale do jej początku. W ten sposób uzyskana póxniej geometria odwrócona jest do góry, a nie do dołu. Zachowanie niewyjaśnione.
-        wierzcholki.insert(0,wspolrzedna_y)
-        wierzcholki.insert(0,wspolrzedna_z)
-        wierzcholki.insert(0,wspolrzedna_x) 
-    #usunięcie ostatniego wierzcholka, będącego zdublowanym pierwszym wierzcholkiem. UWAGA: zachowanie niewyjasnione
-    wierzcholki = wierzcholki[:-3]
-    #tworzymy ksztalt w CityEngine za pomocą listy wspolrzednych
-    ce.createShape(None, wierzcholki)
-print 'koniec'
-print '___________________________'
+def CreateTempDir():
+    #tworzy katalog roboczy /temp/ jeśli nie istnieje
+    if not os.path.exists(tempdir):
+        os.makedirs(tempdir)
+    print 'Temp directory set'
+
+def CleanTempDir():
+    for files in ce.getObjectsFrom(tempdir, ce.isFile):
+        ce.delete(files)
+    print 'Temp directory cleaned'
+
+def SetWorkGeometry():
+    geometry = ce.getObjectsFrom(ce.selection())
+    return geometry
+
+def ConvertToWKT(polygons):    
+    #pusta zmienna, którą będziemy zapełniać listą wierzchołków w formacie WKT. znak /n oznacza załamanie linii
+    geometry_WKT ='WKT,\n'    
+    print len(polygons),"polygons selected"
+    #iterujemy przez zaznaczone obiekty i każdy tłumaczymy na WKT
+    for polygon in polygons:
+        geometry_WKT +='"POLYGON (('
+        #zmienna zawierająca ilość wierzchołków geometrii 
+        num_of_verts = (len(ce.getVertices(polygon))/3) 
+        #zmienna zawierająca zbiór wierzchołków XYZ
+        verts = ce.getVertices(polygon)    
+        #iterujemy przez wierzchołki i odczytujemy tylko wartości X i Y, pomijamy Z
+        for n in range(num_of_verts):            
+            #formuła, która dla każdego n-tego wierzchołka bierze tylko współrzędną 1 i 3, pomijając 2 która jest wysokością
+            x = ce.getVertices(polygon)[3*(n+1)-3]
+            x = round(x,2)
+            #cityengine przechowuje współrzędną Y z minusem. Dla zgodności z szejpami zewnętrzymi trzeba przemnożyć przez -1. Uwaga: przy ładowaniu geometrii z powrotem do CityEngine trzeba przemnozyć przez -1 jeszcze raz
+            y = ce.getVertices(polygon)[3*(n+1)-1]*(-1)
+            y = round(y,2)
+            #dołączamy do zmiennej kolejno wspólrzędną x, spację, współrzędną y i przecinek
+            geometry_WKT +=`x`
+            geometry_WKT +=" "
+            geometry_WKT +=`y`
+            geometry_WKT +=","
+        #ucinamy ostatni przecinek    
+        geometry_WKT = geometry_WKT[:-1]
+        #zamykamy geometrię dwoma nawiasami i cudzysłowiem
+        geometry_WKT +='))"\n'
+    #ustalamy ścieżkę zapisu i nazwę pliku csv
+    output = tempdir+"geometry_WKT.csv"
+    #otwieramy plik z prawami zapisu
+    output_write = open(output, "w")
+    #dodajemy do pliku zawartość zmiennej
+    output_write.write(geometry_WKT)
+    #zamykamy plik
+    output_write.close()
+    print 'saved WKT geometry as CSV'
+    return output
+
+def ConvertWKTtoSHP(input):
+    #ustalamy ścieżkę zapisu i nazwę pliku po konwersji do shp
+    output_name = 'geometry_SHP'
+    output = tempdir+output_name+".shp"
+    #oruchomienie zewnetrznej aplikacji ogr2ogr do konwersji csv na shp
+    os.system("ogr2ogr "+output+" "+input)
+    print 'CSV to SHP conversion successfull'
+    return output,output_name
+
+def AggregateSHP(input,input_name):
+    #dodanie pustej kolumny do umożliwienia agregacji; brudny trick; lepsze byłoby agregowanie wszystkiego z pominięciem tego kroku
+    os.system('ogrinfo '+input+' -sql "ALTER TABLE '+input_name+' ADD COLUMN kolumna character(10)"')
+    print 'Aggregation column added'
+    #ustalamy ścieżkę zapisu i nazwę pliku shp po agregacji
+    output = tempdir+"geometry_agre.shp"
+    os.system("ogr2ogr -explodecollections "+output+" "+input+" -dialect sqlite -sql 'SELECT kolumna,ST_Union(geometry) AS geometry FROM "+input_name+" GROUP BY kolumna'")
+    print 'Aggregation successfull'
+    return output
+    
+def ConvertSHPtoWKT(input):
+    #ustalamy ścieżkę zapisu i nazwę pliku shp po konwersji do csv
+    output = tempdir+"geometry_CSV_agre.csv"
+    #konwersja zagregowanego pliku shp do csv z geometrią WKT
+    os.system("ogr2ogr -f CSV "+output+" "+input+" -lco GEOMETRY=AS_WKT")
+    print 'SHP to CSV successfull'
+    return output
+    
+def ImportWKTPolygons(input):
+    #otwieramy plik z geometrią WKT
+    input_open = open(input, "r")
+    #ustalenie zmiennej w której znajduje się treśc otwartego pliku CSV
+    input_read = input_open.read()
+    #zamykamy plik, nie jest już potrzebny
+    input_open.close()
+    #zamiana linijek tekstu na elementy listy
+    lines_break = re.compile("[^\n]+")
+    #ustalenie zmiennej w której znajduje się lista zrobiona z linijek tekstu
+    lines = lines_break.findall(input_read)
+    #usunięcie pierwszego elementu listy zawierającego ciąg znaków "WKT,kolumna,"
+    lines.pop(0)
+    for polygon in lines:
+        #ustalenie pustej listy zapelnianej wierzcholkami do stworzenia nowej geometrii w CityEngine
+        verts = []
+        #usuwamy zbędne ciągi znaków wynikające z konstrukcji WKT
+        polygon = polygon.replace('"POLYGON ((','')
+        polygon = polygon.replace('))",','')
+        #rozbijamy uzyskany ciąg znaków na listę wierzchołków o konstrukcji x y, x y, x y,...
+        polygon = polygon.split(',')
+        #iterujemy przez każdy wierzchołek poligonu by wydobyć współrzędne wierzchołków
+        for vertex in polygon:
+            #rozbijamy konstrukcję [x y] na dwa elementy [x,y] za pomocą rozdzielającej spacji    
+            vertex = vertex.split(' ')
+            #konwersja tekstu (str) na float
+            coords = [float(coord) for coord in vertex]
+            coord_x = coords[0]
+            #przemnażamy z powrotem współrzędną y przez -1
+            coord_y = coords[1]*(-1)
+            #dodajemy wspolrzedną z
+            coord_z = 0
+            #dodajemy do listy wspolrzednych poligonu uzyskane wspolrzedne wierzcholka. UWAGA: dodajemy w odwrotnej kolejności (yzx, zamiast xzy) oraz nie do końca listy, ale do jej początku. W ten sposób uzyskana póxniej geometria odwrócona jest do góry, a nie do dołu. Zachowanie niewyjaśnione.
+            verts.insert(0,coord_y)
+            verts.insert(0,coord_z)
+            verts.insert(0,coord_x) 
+        #usunięcie ostatniego wierzcholka, będącego zdublowanym pierwszym wierzcholkiem. UWAGA: zachowanie niewyjasnione
+        verts = verts[:-3]
+        #tworzymy ksztalt w CityEngine za pomocą listy wspolrzednych
+        ce.createShape(None, verts)
+        print 'WKT import as shape successfull'
+
+if __name__ == '__main__':
+    CreateTempDir()
+    CleanTempDir()
+    geometry = SetWorkGeometry()
+    geometry_CSV = ConvertToWKT(geometry)
+    geometry_SHP, geometry_SHP_name = ConvertWKTtoSHP(geometry_CSV)
+    geometry_SHP_agre = AggregateSHP(geometry_SHP,geometry_SHP_name)
+    geometry_CSV_agre = ConvertSHPtoWKT(geometry_SHP_agre)
+    ImportWKTPolygons(geometry_CSV_agre)
